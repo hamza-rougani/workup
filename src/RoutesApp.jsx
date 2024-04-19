6ec5c5df41c586f828a07c2767ed9fa6ab9832b6
hamza <hamzarougani2000@gmail.com.com>
2024-04-20 00:53:57 +0100

    change

--- src/RoutesApp.jsx
+++ src/RoutesApp.jsx
@@ -5,6 +5,8 @@ import Dashboard from './Views/Admin/Dashboard'
 import Guest_Layout from './Layouts/Guest_Layout'
 import SingleProduct from './Views/ProductPage/SingleProduct'
 import ReactGa from 'react-ga'
+const trackingCode = '437325189'
+ReactGa.initialize(trackingCode)
 function RoutesApp() {
   return (
     <Routes>
--- src/Views/ProductPage/SingleProduct.jsx
+++ src/Views/ProductPage/SingleProduct.jsx
@@ -1,19 +1,14 @@
 import { useEffect } from 'preact/hooks'
 import React from 'react'
+import ReactGa from 'react-ga'
 
 function SingleProduct() {
     useEffect(()=>{
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PN5J8VWX');
+ReactGa.pageview(window.location.pathname)
     },[])
   return (
-    <div className='SingleProduct'>
-      	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PN5J8VWX"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
+    <div>
+  welcome to single product
     </div>
 
   )
