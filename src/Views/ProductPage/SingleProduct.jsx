a85fc87ee6f0c67ca6e541b9234306c1a8e455bb
hamza <hamzarougani2000@gmail.com.com>
2024-04-20 00:08:49 +0100

    change

--- src/Views/ProductPage/SingleProduct.jsx
+++ src/Views/ProductPage/SingleProduct.jsx
@@ -1,10 +1,21 @@
+import { useEffect } from 'preact/hooks'
 import React from 'react'
 
 function SingleProduct() {
+    useEffect(()=>{
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PN5J8VWX');
+    },[])
   return (
     <div className='SingleProduct'>
-        hello everyone
+      	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PN5J8VWX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
     </div>
+
   )
 }
 
